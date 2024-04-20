7be9b850ab515870953b4d16c5d0ad78a8d2a06e
mori8a <morazul60@gmail.com>
2024-04-20 13:20:08 +0300

    new script

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-console.log ('hello world')
-console.log ('testing')
+function createNameDiv(firstName, lastName) {
+    let main = document.createElement("main");
+
+    let firstParagraph = document.createElement("p");
+    let firstName = document.createText(firstName);
+    firstParagraph.appendChild(firstName);
+
+    let secondParagraph = document.createElement("p");
+    let lastName = document.createText(lastName);
+    secondParagraph.appendChild(lastName);
+
+    main.appendChild(firstParagraph);
+    main.appendChild(secondParagraph);
+
+    document.body.appendChild(main);
+}
+
+
+
